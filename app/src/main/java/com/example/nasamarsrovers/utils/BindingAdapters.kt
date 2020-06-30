97de8c81fa6293c0ed61c4f6b8b3428be9ae8929
Szczepan <l.szczepan@wp.pl>
2020-06-30 11:21:33 +0200

    placing sol and earth date info on photo card

--- app/src/main/java/com/example/nasamarsrovers/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/nasamarsrovers/utils/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.example.nasamarsrovers.utils
 
 import android.widget.ImageView
 import android.widget.NumberPicker
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.nasamarsrovers.R
@@ -22,5 +23,17 @@ object BindingAdapters {
     @BindingAdapter("value")
     fun NumberPicker.bindValue(value: Int) {
         this.value = value
+    }
+
+    @JvmStatic
+    @BindingAdapter("earthDate")
+    fun TextView.earthDate(earthDate: String) {
+        text = resources.getString(R.string.earth_date, earthDate )
+    }
+
+    @JvmStatic
+    @BindingAdapter("sol")
+    fun TextView.sol(sol: Int) {
+        text = resources.getString(R.string.sol, sol)
     }
 }
